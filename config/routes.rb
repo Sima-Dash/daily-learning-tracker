1e507ddd5f92294fc8f9a13e0cbcbbe9d10c31d1
Sima-Dash <simadashpuri@gmail.com>
2026-06-18 11:32:21 +0530

    Updated routes for CRUD flow

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   root "learning_entries#index"
 
-  get "/learning_entries", to: "learning_entries#index"
-
-  get "/learning_entries/new", to: "learning_entries#new"
+  resources :learning_entries
   
 end
