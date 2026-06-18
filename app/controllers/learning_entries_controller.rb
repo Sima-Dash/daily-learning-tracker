4055afaabb47679fe33a1846bedc9f551bfa6201
Sima-Dash <simadashpuri@gmail.com>
2026-06-18 11:22:47 +0530

    Implemented create functionality

--- app/controllers/learning_entries_controller.rb
+++ app/controllers/learning_entries_controller.rb
@@ -1,7 +1,32 @@
 class LearningEntriesController < ApplicationController
     def index
+        @entries = LearningEntry.all
     end
     
     def new
+        @entry = LearningEntry.new
     end
+
+    def create
+        @entry= LearningEntry.new(entry_params)
+        
+        if @entry.save
+            redirect_to learning_entries_path
+        
+        end
+    end
+
+    private
+
+    def entry_params
+        params
+            .required(:learning_entry)
+            .permit(
+                :date,
+                :topic,
+                :hours,
+                :notes
+            )
+    end
+
 end
