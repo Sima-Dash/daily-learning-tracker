9a5253f442508fe8e3e8b50f6fcf3c1c35c8bdce
Sima-Dash <simadashpuri@gmail.com>
2026-07-10 12:22:46 +0530

    Improve UI using Bootstrap components

--- app/models/learning_entry.rb
+++ app/models/learning_entry.rb
@@ -2,4 +2,5 @@ class LearningEntry < ApplicationRecord
     validates :date, presence: true
     validates :topic, presence: true
     validates :hours, presence: true
+    validates :notes, presence: true
 end
